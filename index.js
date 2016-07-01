4516a75dd40782e154f47e08020f3d8a5debb4a2
Evan Czaplicki <evancz@users.noreply.github.com>
2016-07-01 13:45:03 -0700

    First draft of implementation

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
-//
-// import 'react-elm' as Elm;
-// import '../elm/my-stuff.js' as Elm
-//
-// <Elm src={Elm.MyStuff} flags={42} ports={setupPorts} />
-//
+var React = require('react');
 
-var app = app || {};
+module.exports = React.createClass({
 
-(function () {
+	initialize: function(node)
+	{
+		var app = this.props.src.embed(node, this.props.flags);
 
-'use strict';
-
-var TodoItem = React.createClass({
-
-	getInitialState: function () {
-		this.props.src;
-		this.props.flags
-		return;
+		if (typeof this.props.ports !== 'undefined')
+		{
+			this.props.ports(app.ports);
+		}
 	},
 
-	shouldComponentUpdate: function (nextProps, nextState) {
+	shouldComponentUpdate: function(prevProps)
+	{
+		if (prevProps.src !== this.props.src)
+		{
+			return true;
+		}
+
+		if (typeof this.props.flags !== 'undefined' && prevProps.flags !== this.props.flags)
+		{
+			console.error(
+				'You changed the `flags` property of an Elm component. '
+				+ 'Flags are only useful on initialization, so it was ignored.'
+			);
+			return false;
+		}
+
+		if (typeof this.props.ports !== 'undefined' && prevProps.ports !== this.props.ports)
+		{
+			console.error(
+				'You changed the `ports` property of an Elm component. '
+				+ 'You can only setup ports at initialization, so it was ignored.'
+			);
+			return false;
+		}
 	},
 
-	componentDidUpdate: function (prevProps) {
-	},
-
-	render: function () {
+	render: function ()
+	{
+		return <div ref={this.initialize} />
 	}
 
 });
-
-})();
