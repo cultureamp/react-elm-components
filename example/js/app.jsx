9cca6dc542cac45cf67da459a0f5166e63f95649
Evan Czaplicki <evancz@users.noreply.github.com>
2016-07-01 15:55:13 -0700

    Potential implementation of EmojiChatRoom

--- example/js/app.jsx
+++ example/js/app.jsx
@@ -1,28 +1,31 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
 var EmojiPicker = require('emojione-picker');
 var Elm = require('react-elm');
 var Chat = require('../dist/elm-chat.js');
 
+var EmojiChatRoom = React.createClass({
 
-emoji: { send: function() {} }
+	render: function() {
+		var flags = 'wss://echo.websocket.org';
 
-function render()
-{
-	var flags = 'wss://echo.websocket.org';
+		return (
+			<div class="chat">
+				<Elm src={Chat} flags={flags} ports={this.setupPorts} />
+				<EmojiPicker onChange={this.sendEmojiToChat} />
+			</div>
+		);
+	},
 
-	return (
-		<div class="chat">
-			<Elm src={Chat} flags={flags} ports={this.setupPorts} />
-			<EmojiPicker onChange={this.sendEmojiToChat} />
-		</div>
-	);
-}
+	sendEmojiToChat: function(emoji) {},
 
-function setupPorts(ports)
-{
-	this.emoji = ports.emoji;
-}
+	setupPorts: function(ports) {
+		this.sendEmojiToChat = ports.emoji.send;
+	},
 
-function sendEmojiToChat(emoji)
-{
-	this.emoji.send(emoji);
-}
+});
+
+ReactDOM.render(
+	<EmojiChatRoom />,
+	document.getElementById('chat');
+);
