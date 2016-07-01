a980d78579e9607b088289cc207d4555d0f5ebbc
Evan Czaplicki <evancz@users.noreply.github.com>
2016-07-01 13:55:17 -0700

    Never update, all managed by Elm after initialization
    On @rtfeldman’s advice, changing properties will not change anything.
    Create a brand new component, perhaps with a different key if you want
    it to replace the old one.

--- index.js
+++ index.js
@@ -14,33 +14,12 @@ module.exports = React.createClass({
 
 	shouldComponentUpdate: function(prevProps)
 	{
-		if (prevProps.src !== this.props.src)
-		{
-			return true;
-		}
-
-		if (typeof this.props.flags !== 'undefined' && prevProps.flags !== this.props.flags)
-		{
-			console.error(
-				'You changed the `flags` property of an Elm component. '
-				+ 'Flags are only useful on initialization, so it was ignored.'
-			);
-			return false;
-		}
-
-		if (typeof this.props.ports !== 'undefined' && prevProps.ports !== this.props.ports)
-		{
-			console.error(
-				'You changed the `ports` property of an Elm component. '
-				+ 'You can only setup ports at initialization, so it was ignored.'
-			);
-			return false;
-		}
+		return false;
 	},
 
 	render: function ()
 	{
-		return <div ref={this.initialize} />
+		return React.createElement('div', { ref: this.initialize });
 	}
 
 });
