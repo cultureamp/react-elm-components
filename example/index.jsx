9c73131ba66c48170d930ef576485a5490df5352
Richard Feldman <richard.t.feldman@gmail.com>
2016-07-01 17:36:28 -0700

    Got it working.

--- example/index.jsx
+++ example/index.jsx
@@ -9,20 +9,25 @@ var EmojiChatRoom = React.createClass({
   render: function() {
     var flags = 'wss://echo.websocket.org';
 
+    var sendEmojiToChat = function(emoji) {};
+
+    var setupPorts = function(ports) {
+      sendEmojiToChat = ports.emoji.send;
+    };
+
+    function handleChange(emoji) {
+      var str = String.fromCodePoint(parseInt("0x" + emoji.unicode));
+
+      sendEmojiToChat(str);
+    }
+
     return (
-      <div className="chat">
-        <Elm src={Chat} flags={flags} ports={this.setupPorts} />
-        <EmojiPicker onChange={this.sendEmojiToChat} />
+      <div className="emoji-chat">
+        <Elm src={Chat} flags={flags} ports={setupPorts} />
+        <EmojiPicker onChange={handleChange} />
       </div>
     );
-  },
-
-  sendEmojiToChat: function(emoji) {},
-
-  setupPorts: function(ports) {
-    this.sendEmojiToChat = ports.emoji.send;
-  },
-
+  }
 });
 
 ReactDOM.render(
