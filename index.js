22ae945f7c757d90fcce5277c21b202619548a4e
argshook <argshook@gmail.com>
2017-06-03 21:12:49 +0300

    chore(index): use createReactClass

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var React = require('react');
+var createReactClass = require('create-react-class');
 
-module.exports = React.createClass({
+module.exports = createReactClass({
 	initialize: function(node) {
 		if (node === null) return;
 		var app = this.props.src.embed(node, this.props.flags);
