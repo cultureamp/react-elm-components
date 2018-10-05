5032fe2fc42325b4e6cbae69a5ae8b443f561585
Russell Mays <russell.c.mays@gmail.com>
2018-10-05 09:38:52 -0700

    switched embed to init

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var createReactClass = require('create-react-class');
 module.exports = createReactClass({
 	initialize: function(node) {
 		if (node === null) return;
-		var app = this.props.src.embed(node, this.props.flags);
+		var app = this.props.src.init({node: node, flags: this.props.flags});
 
 		if (typeof this.props.ports !== 'undefined') {
 			this.props.ports(app.ports);
